0a1c614b68f99b225a6fc619413a444783739b10
Harold Padilla <hypadilla@hotmail.com>
2024-10-18 23:21:06 -0500

    init

---
+++ MeliChallenge/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  MeliChallenge
+//
+//  Created by Harold Padilla on 18/10/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
